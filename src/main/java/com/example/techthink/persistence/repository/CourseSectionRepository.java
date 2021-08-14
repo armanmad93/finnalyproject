32d91bd4507d3db8680ff1aa99b7988ba82b53d3
Mery <meri_minasyan@edu.aua.am>
2021-08-14 15:58:30 +0400

    August 14 last version

---
+++ src/main/java/com/example/techthink/persistence/repository/CourseSectionRepository.java
@@ -0,0 +1,7 @@
+package com.example.techthink.persistence.repository;
+
+import com.example.techthink.persistence.CourseSection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseSectionRepository extends JpaRepository<CourseSection, Long> {
+}
